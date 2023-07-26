2a192339a77a2f786ed17e8e68e7445ab87a0c28
Yoni A <themarketwizard1@gmail.com>
2023-07-26 21:52:20 +0300

    Enable toggle from keyboard when popup is open

--- extension/popup.js
+++ extension/popup.js
@@ -1,37 +1,39 @@
+// page state
 let darkModeActive = false;
-const toggleElement = document.querySelector(".toggle--checkbox");
-// toggleElement.checked = true;
 
-// alert("dark mode active? " + darkModeActive);
-
+// constants
+const TOGGLE_KEY = "i";
+const toggleButton = document.querySelector("#toggle-button");
 
 
 // When the button is clicked, inject togglePageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    // togglePageBackgroundColor(); // for button
-    
-    // count = count + 1;
-    // alert(count);
+toggleButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: togglePageBackgroundColor,
+        function: togglePageBackgroundColor
     });
 });
+
+
+document.addEventListener("keyup", (event) => toggleFromKeyboard(event));
   
-  // The body of this function will be executed as a content script inside the
-  // current page
   function togglePageBackgroundColor() {
     this.darkModeActive = !this.darkModeActive;
     document.body.style.filter = darkModeActive ? "invert(1)" : "invert(0)";
     document.body.style.backgroundColor = darkModeActive ? "black" : "white";
-
-    // toggle button color
-    // toggleElement = document.querySelector(".toggle--checkbox");
-    // toggleElement.checked = this.darkModeActive;
   }
 
+  // activate toggle button without mouse
+  async function toggleFromKeyboard(event) {
+    if (event.key === TOGGLE_KEY) {
+      toggleButton.checked = true;
+      let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          function: togglePageBackgroundColor
+      });
+    }
+  }
 
-  // function toggleButtonColor() {
-
-  // }
+  
