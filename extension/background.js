7c8ec7f2432d3a07928dc978944a42bc7a4b938b
Yoni A <themarketwizard1@gmail.com>
2023-07-27 19:25:55 +0300

    Persist dark mode state in local storage; Fix initial toggle animation state

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,32 @@
-let color = '#3aa757';
+chrome.runtime.onInstalled.addListener(setInitialState);
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === "setState") {
+      console.log(message);
+      const state = message.darkModeActive;
+      // Store data in chrome.storage.local
+      chrome.storage.local.set({ "darkModeActive": state }, function () {
+        console.log("Data set in storage:", state);
+      });
+    } else if (message.action === "getState") {
+      // Retrieve data from chrome.storage.local
+      chrome.storage.local.get("darkModeActive", function (result) {
+        console.log("Data retrieved from storage:", result.darkModeActive);
+        sendResponse({ result }); // Send data back to popup
+      });
+      return true; // To indicate that the response will be sent asynchronously
+    }
+  });
+
+
+function setInitialState() {
+  chrome.storage.local.set({ "darkModeActive": "false" }, function () {
+    console.log("Set initial state in storage:", "false");
+  });
+}
+
+
+// page change
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log(JSON.stringify(changeInfo));
 });
--- extension/popup.js
+++ extension/popup.js
@@ -1,39 +1,62 @@
-// page state
-let darkModeActive = false;
+// set initial state based on existing storage value
+getState();
 
 // constants
 const TOGGLE_KEY = "i";
 const toggleButton = document.querySelector("#toggle-button");
 
-
 // When the button is clicked, inject togglePageBackgroundColor into current page
 toggleButton.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    setState(toggleButton.checked.toString());
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: togglePageBackgroundColor
+        function: togglePageBackgroundColor,
+        args: [toggleButton.checked]
     });
 });
 
 
 document.addEventListener("keyup", (event) => toggleFromKeyboard(event));
-  
-  function togglePageBackgroundColor() {
-    this.darkModeActive = !this.darkModeActive;
-    document.body.style.filter = darkModeActive ? "invert(1)" : "invert(0)";
-    document.body.style.backgroundColor = darkModeActive ? "black" : "white";
-  }
 
-  // activate toggle button without mouse
-  async function toggleFromKeyboard(event) {
-    if (event.key === TOGGLE_KEY) {
-      toggleButton.checked = true;
-      let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-      chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          function: togglePageBackgroundColor
-      });
-    }
-  }
 
-  
+function togglePageBackgroundColor(darkModeActive) {
+  // this.darkModeActive = !this.darkModeActive;
+  document.body.style.filter = darkModeActive ? "invert(1)" : "invert(0)";
+  document.body.style.backgroundColor = darkModeActive ? "black" : "white";
+}
+
+// activate toggle button without mouse
+async function toggleFromKeyboard(event) {
+  if (event.key === TOGGLE_KEY) {
+    toggleButton.checked = !toggleButton.checked;
+    setState(toggleButton.checked.toString());
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: togglePageBackgroundColor,
+        args: [toggleButton.checked]
+    });
+  }
+}
+
+
+// background service messages
+
+function setState(darkModeActive) {
+  console.log("sending data to service");
+  chrome.runtime.sendMessage({ action: "setState", darkModeActive });
+}
+
+function getState() {
+  chrome.runtime.sendMessage({ action: "getState" }, async (response) => {
+    const darkModeActive = response.result.darkModeActive;
+    toggleButton.checked = darkModeActive === "true" ? true: false;
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: togglePageBackgroundColor,
+        args: [toggleButton.checked]
+    });
+  });
+}
